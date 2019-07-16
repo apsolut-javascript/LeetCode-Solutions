8e1c3b3ff6cf124ec0ac3a072e0a9b33ce5a82aa
Peter Liang <peter_liang@outlook.com>
2019-07-16 17:55:53 +0800

    Optimmized the solution of problem 16.

--- solutions/16.3-sum-closest.js
+++ solutions/16.3-sum-closest.js
@@ -9,30 +9,31 @@
  * @return {number}
  */
 var threeSumClosest = function(nums, target) {
-  const n = nums.sort((a, b) => a - b)
+  nums = nums.sort((a, b) => a - b)
   let nearest = Number.POSITIVE_INFINITY
   let result = null
-  for (let i = 0; i < n.length - 1; i++) {
-    if (i > 0 && n[i - 1] === n[i]) continue
-    const x = n[i]
-
-    for (let end = n.length - 1; i + 1 < end; end--) {
-      if (end < n.length - 1 && n[end] === n[end + 1]) continue
-      const z = n[end]
-
-      let distance = Number.POSITIVE_INFINITY
-      for (let start = i + 1; start < end; start++) {
-        if (start > i + 1 && n[start] === n[start - 1]) continue
-        const y = n[start]
-        const sum = x + y + z
-        const d = Math.abs(sum - target)
-        if (d > distance) break
-
-        distance = d
-        if (distance < nearest) {
-          nearest = distance
-          result = sum
-        }
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (i > 0 && nums[i - 1] === nums[i]) continue
+    const x = nums[i]
+    let start = i + 1
+    let end = nums.length - 1
+    while (start < end) {
+      const sum = x + nums[start] + nums[end]
+      if (sum === target) {
+        return target
+      } else if (sum > target) {
+        do {
+          end--
+        } while (nums[end] === nums[end + 1] && start < end)
+      } else {
+        do {
+          start++
+        } while (nums[start] === nums[start - 1] && start < end)
+      }
+      const distance = Math.abs(sum - target)
+      if (distance < nearest) {
+        nearest = distance
+        result = sum
       }
     }
   }
