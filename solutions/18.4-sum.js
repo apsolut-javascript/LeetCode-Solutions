f3c2bb8bc7fe2b664d5b2cd2f46e6637f2cc5ddc
Peter Liang <peter_liang@outlook.com>
2019-07-18 14:50:06 +0800

    A better solution of problem 18.

--- solutions/18.4-sum.js
+++ solutions/18.4-sum.js
@@ -12,41 +12,35 @@ var fourSum = function(nums, target) {
   if (nums.length < 4) return []
   nums.sort((a, b) => a - b)
   const result = []
-  const dict = nums.reduce((p, c) => (p[c] ? (p[c] += 1) : (p[c] = 1), p), {})
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < nums.length - 3; i++) {
     const x = nums[i]
-    for (let j = i + 1; j < nums.length; j++) {
+    for (let j = i + 1; j < nums.length - 2; j++) {
       const y = nums[j]
       const remain = target - x - y
       if (remain > nums.slice(-2).reduce((p, c) => p + c, 0)) continue
-      for (let k = nums.length - 1; k > j; k--) {
-        const z = nums[k]
-        const sum = x + y + z
-        const differ = target - sum
-        if (differ > z) break
-        if (!dict[differ]) continue
-        const solution = [x, y, z, differ]
-        const constitute = solution.reduce(
-          (p, c) => (p[c] ? (p[c] += 1) : (p[c] = 1), p),
-          {}
-        )
-        const isValid = Object.entries(constitute).every(
-          ([k, v]) => dict[k] >= v
-        )
-        if (!isValid) continue
-
-        solution.sort((a, b) => a - b)
-        if (
-          result.some(
-            a =>
-              a[0] == solution[0] &&
-              a[1] == solution[1] &&
-              a[2] == solution[2] &&
-              a[3] == solution[3]
-          )
-        )
-          continue
-        result.push(solution)
+      let start = j + 1
+      let end = nums.length - 1
+      while (start < end) {
+        const l = nums[start]
+        const r = nums[end]
+        const diff = remain - l - r
+        if (diff < 0) {
+          do {
+            end--
+          } while (nums[end] == nums[end + 1] && start < end)
+        } else if (diff > 0) {
+          do {
+            start++
+          } while (nums[start - 1] == nums[start] && start < end)
+        } else {
+          if (
+            result.every(a => a[0] != x || a[1] != y || a[2] != l || a[3] != r)
+          ) {
+            result.push([x, y, l, r])
+          }
+          start++
+          end--
+        }
       }
     }
   }
--- test/18.4-sum.test.js
+++ test/18.4-sum.test.js
@@ -13,4 +13,20 @@ describe("problem 18", () => {
     expect(result).toHaveLength(expected.length)
     result.forEach(a => expect(expected).toContainEqual(a))
   })
+
+  test("case 3", () => {
+    const expected = [
+      [-3, -2, 2, 3],
+      [-3, -1, 1, 3],
+      [-3, 0, 0, 3],
+      [-3, 0, 1, 2],
+      [-2, -1, 0, 3],
+      [-2, -1, 1, 2],
+      [-2, 0, 0, 2],
+      [-1, 0, 0, 1],
+    ]
+    const result = fourSum([-3, -2, -1, 0, 0, 1, 2, 3], 0)
+    expect(result).toHaveLength(expected.length)
+    result.forEach(a => expect(expected).toContainEqual(a))
+  })
 })
