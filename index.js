9be0c917e18927c04bfe0d6a3a75ac4eedcdd7b3
Peter Liang <peter_liang@outlook.com>
2019-07-24 16:19:50 +0800

    Added debug code.

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 import debug from "debug"
 
-debug("a")("test")
+const log = debug("index")
+const a = new Set()
+const size = 1000000
+
+log("warmup start")
+for (let i = 0; i < size; i++) {
+  a.add(i)
+}
+a.clear()
+
+log("warmup end")
+
+log("normal start")
+for (let i = 0; i < size; i++) {
+  a.add(i)
+}
+for (let i = 0; i < size; i++) {
+  a.add(i)
+}
+a.clear()
+
+log("normal end")
+
+log("checking start")
+for (let i = 0; i < size; i++) {
+  a.add(i)
+}
+for (let i = 0; i < size; i++) {
+  if (!a.has(i)) a.add(i)
+}
+a.clear()
+
+log("checking end")
