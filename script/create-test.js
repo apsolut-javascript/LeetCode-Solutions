cc50e04e4180641ffc55b5aeb34d0e03041571a9
Peter Liang <peter_liang@outlook.com>
2019-08-02 11:21:26 +0800

    Updated create test script. Ignored index.js.

--- script/create-test.js
+++ script/create-test.js
@@ -3,35 +3,44 @@ import fse from "fs-extra"
 const templatePath = "./script/test-template"
 
 async function main() {
-  const latestSolution = await fse.readdir("solutions/").then(getLatestFile)
-  const latestTest = await fse.readdir("test/").then(getLatestFile)
-  if (latestSolution.num == latestTest.num) {
+  const solutions = await fse.readdir("solutions/").then(getJsFiles)
+  const tests = await fse.readdir("test/").then(getJsFiles)
+
+  const newSolutions = solutions.filter(a => tests.every(b => b.num != a.num))
+  if (newSolutions.length == 0) {
     console.log("No new solution found!")
     return
   }
 
-  const data = await fse
-    .readFile(templatePath, "utf8")
-    .then(d =>
-      d
-        .replace("$1", `../solutions/${latestSolution.name.replace(".js", "")}`)
-        .replace("$2", latestSolution.num)
-    )
+  const promisies = newSolutions.map(async a => {
+    const data = await fse
+      .readFile(templatePath, "utf8")
+      .then(d =>
+        d
+          .replace("$1", `../solutions/${a.name.replace(".js", "")}`)
+          .replace("$2", a.num)
+      )
 
-  const testPath = `./test/${latestSolution.name.replace(".js", ".test.js")}`
-  await fse.writeFile(testPath, data)
-  console.log(`New test created successfully: ${testPath}`)
+    const testPath = `./test/${a.name.replace(".js", ".test.js")}`
+    await fse.writeFile(testPath, data)
+    console.log(`New test created successfully: ${testPath}`)
+  })
+
+  await Promise.all(promisies)
+  console.log("All test files created!")
 }
 
-async function getLatestFile(files) {
-  const reg = /\d+/
+async function getJsFiles(files) {
+  const reg = /^(\d+).*\.js$/
   return files
     .filter(a => reg.test(a))
     .map(a => ({
-      num: Number(a.match(reg)[0]),
+      num: Number(a.match(reg)[1]),
       name: a,
     }))
-    .reduce((p, c) => (p.num < c.num ? c : p), { num: 0 })
 }
 
-main()
+main().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
