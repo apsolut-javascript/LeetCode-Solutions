e002932eabbd55873b745db8e34af40da7cc6626
Peter Liang <peter_liang@outlook.com>
2019-08-27 17:58:35 +0800

    Updated solution 63. [52ms, 94.81%, 100%]

--- solutions/63.unique-paths-ii.js
+++ solutions/63.unique-paths-ii.js
@@ -11,19 +11,25 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
   if (obstacleGrid.length == 0) return 0
   if (obstacleGrid[0][0] == 1) return 0
 
-  obstacleGrid[0][0] = 1
-  for (let i = 1; i < obstacleGrid[0].length; i++) {
-    obstacleGrid[0][i] = obstacleGrid[0][i] == 0 ? obstacleGrid[0][i - 1] : 0
-  }
-
-  for (let i = 1; i < obstacleGrid.length; i++) {
-    obstacleGrid[i][0] = obstacleGrid[i][0] == 0 ? obstacleGrid[i - 1][0] : 0
-  }
-
-  for (let i = 1; i < obstacleGrid.length; i++) {
-    for (let j = 1; j < obstacleGrid[0].length; j++) {
+  for (let i = 0; i < obstacleGrid.length; i++) {
+    for (let j = 0; j < obstacleGrid[0].length; j++) {
       if (obstacleGrid[i][j] == 1) {
         obstacleGrid[i][j] = 0
+        continue
+      }
+
+      if (i == 0) {
+        if (j == 0) {
+          obstacleGrid[i][j] = 1
+        } else {
+          obstacleGrid[i][j] = obstacleGrid[i][j - 1]
+        }
+
+        continue
+      }
+
+      if (j == 0) {
+        obstacleGrid[i][j] = obstacleGrid[i - 1][j]
         continue
       }
 
