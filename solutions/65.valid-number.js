230d2ed251f13addb3914b23f88d2edefaca56ba
Peter Liang <peter_liang@outlook.com>
2019-08-28 15:53:10 +0800

    Optimized problem 65.

--- solutions/65.valid-number.js
+++ solutions/65.valid-number.js
@@ -4,25 +4,12 @@
  * [65] Valid Number
  */
 
-const baseReg = /^[+-]?(\d*\.?\d+|\d+\.)$/
-const exponentReg = /^[+-]?\d+$/
-
 /**
  * @param {string} s
  * @return {boolean}
  */
 var isNumber = function(s) {
-  s = s.trim()
-  if (s.length == 0) return false
-
-  const [base, exponent, other] = s.split("e")
-  if (other != undefined) return false
-  if (!baseReg.test(base)) return false
-  if (exponent != undefined) {
-    if (!exponentReg.test(exponent)) return false
-  }
-
-  return true
+  return /^\s*[+-]?(\d*\.?\d+|\d+\.)(e[+-]?\d+)?\s*$/.test(s)
 }
 
 module.exports = isNumber
