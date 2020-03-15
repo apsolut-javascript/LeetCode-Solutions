4f827610833ec6931b9f103d52ca7c2208e0a5a0
Peter Liang <peter_liang@outlook.com>
2020-03-15 21:46:01 +0800

    Solved problem 77.

--- solutions/77.combinations.js
+++ solutions/77.combinations.js
@@ -11,57 +11,20 @@
  * @return {number[][]}
  */
 var combine = function(n, k) {
-  const nums = Array.from({ length: n }).map((_, i) => i + 1);
-  if (k == 1) {
-    return nums.map(a => [a]);
-  }
-
-  const result = recurGenerateNums(nums, k);
-  result.sort((a, b) => {
-    for (let i = 0; i < k; i++) {
-      if (a[i] != b[i]) return a[i] - b[i];
-    }
-
-    return 0;
-  });
-  outer: for (let i = result.length - 1; i > 0; i--) {
-    const cur = result[i];
-    const next = result[i - 1];
-    for (let j = 0; j < k; j++) {
-      if (cur[j] != next[j]) {
-        continue outer;
-      }
-    }
-    result.splice(i, 1);
-  }
-  return result;
+  return recurGenerateNums(1, n, k);
 };
 
-function combineNums(nums) {
+function recurGenerateNums(start, end, n) {
   const result = [];
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      result.push([nums[i], nums[j]]);
-    }
-  }
-  return result;
-}
-
-function recurGenerateNums(nums, n) {
-  const result = [];
-  if (n > 2) {
-    for (let i = 0; i < nums.length; i++) {
-      const remainNums = [...nums];
-      const currentNum = remainNums.splice(i, 1)[0];
-      const newCombination = recurGenerateNums(remainNums, n - 1);
-      newCombination.forEach(a => {
-        a.push(currentNum);
-        a.sort();
+  if (n > 1) {
+    for (let i = start; i <= end; i++) {
+      recurGenerateNums(i + 1, end, n - 1).forEach(a => {
+        a.unshift(i);
+        result.push(a);
       });
-      result.push(...newCombination);
     }
   } else {
-    result.push(...combineNums(nums));
+    return Array.from({ length: end - start + 1 }).map((_, i) => [start + i]);
   }
 
   return result;
