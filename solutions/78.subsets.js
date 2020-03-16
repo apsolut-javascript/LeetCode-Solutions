ba7098facefa4ea65c11e8b7d236f807f1ea1193
Peter Liang <peter_liang@outlook.com>
2020-03-16 22:37:39 +0800

    Solved problem 78. [64ms 42.58%, 5.88% 35.7MB]

--- solutions/78.subsets.js
+++ solutions/78.subsets.js
@@ -10,45 +10,31 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
-  const result = [];
   nums.sort();
-  recurSubset(nums).forEach(a => result.push(a));
-  result.sort((a, b) => {
-    const len = Math.min(a.length, b.length);
-    for (let i = 0; i < len; i++) {
-      if (a[i] != b[i]) return a[i] - b[i];
-    }
-
-    return a.length - b.length;
-  });
-  for (let i = result.length - 1; i > 0; i--) {
-    const cur = result[i];
-    const next = result[i - 1];
-    if (cur.length != next.length) continue;
-
-    if (cur.every((a, i) => a == next[i])) {
-      result.splice(i, 1);
-    }
+  const result = [[], nums];
+  for (let i = 1; i < nums.length; i++) {
+    getSubsets(nums, i).forEach(a => result.push(a));
   }
-
-  result.push([]);
 
   return result;
 };
 
-function recurSubset(nums) {
-  if (nums.length > 1) {
-    const result = [nums];
-    for (let i = 0; i < nums.length; i++) {
-      const sub = recurSubset(nums.slice(0, i).concat(nums.slice(i + 1)));
-      sub.forEach(a => result.push(a));
+function getSubsets(nums, n) {
+  if (n > 1) {
+    const result = [];
+    for (let i = 0; i < nums.length - n + 1; i++) {
+      const cur = nums[i];
+      getSubsets(nums.slice(i + 1), n - 1).forEach(a => {
+        a.unshift(cur);
+        result.push(a);
+      });
     }
 
     return result;
   }
 
-  return [nums];
+  return nums.map(a => [a]);
 }
 // @lc code=end
 
-export default subsets;
+// export default subsets;
