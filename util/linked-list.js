5e32eaccf09d2538d3ec30c4bb3570cb364f9a1d
Peter Liang <peter_liang@outlook.com>
2020-03-25 18:23:00 +0800

    Added comment.

--- util/linked-list.js
+++ util/linked-list.js
@@ -1,27 +1,33 @@
 function ListNode(val) {
-  this.val = val
-  this.next = null
+  this.val = val;
+  this.next = null;
 }
 
+/**
+ * Create link list from array
+ */
 const createList = vals => {
-  const head = new ListNode(null)
-  let tail = head
+  const head = new ListNode(null);
+  let tail = head;
   vals.forEach(a => {
-    const node = new ListNode(a)
-    tail.next = node
-    tail = node
-  })
-  return head.next
-}
+    const node = new ListNode(a);
+    tail.next = node;
+    tail = node;
+  });
+  return head.next;
+};
 
+/**
+ * Change link list to array
+ */
 const toList = head => {
-  const list = []
-  let node = head
+  const list = [];
+  let node = head;
   while (node != null) {
-    list.push(node.val)
-    node = node.next
+    list.push(node.val);
+    node = node.next;
   }
-  return list
-}
+  return list;
+};
 
-export { createList, toList }
+export { createList, toList };
