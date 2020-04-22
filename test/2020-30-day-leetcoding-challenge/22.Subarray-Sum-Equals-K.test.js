c4d4ae91009481ad78be3b5110bc0fd2d0446941
Peter Liang <peter_liang@outlook.com>
2020-04-22 19:30:14 +0800

    Solved 2020 30 days leetcoding challenge problem 22.

---
+++ test/2020-30-day-leetcoding-challenge/22.Subarray-Sum-Equals-K.test.js
@@ -0,0 +1,7 @@
+import target from "../../solutions/2020-30-day-leetcoding-challenge/22.Subarray-Sum-Equals-K";
+
+describe("tests for 2020-30-day-leetcoding-challenge problem 21", () => {
+  test("case 1", () => {
+    expect(target([1, 1, 1], 2)).toBe(2);
+  });
+});
