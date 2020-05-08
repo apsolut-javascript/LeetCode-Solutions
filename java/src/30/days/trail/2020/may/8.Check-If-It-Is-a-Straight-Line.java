e075448a0c00e37b3b4093006bc9511b4a102a77
Peter Liang <peter_liang@outlook.com>
2020-05-08 18:19:05 +0800

    Solved problem may 2020 30 days trail problem 8.

---
+++ java/src/30/days/trail/2020/may/8.Check-If-It-Is-a-Straight-Line.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public static void main(String[] args) {
+        int[][] input = {{-4, -3}, {1, 0}, {3, -1}, {0, -1}, {-5, 2}};
+        System.out.println(Solution.checkStraightLine(input));
+    }
+
+    public static boolean checkStraightLine(int[][] coordinates) {
+        double slope = ((double) (coordinates[1][1] - coordinates[0][1])) / ((double) (coordinates[1][0] - coordinates[0][0]));
+
+        for (int i = 2; i < coordinates.length; i++) {
+            double curSlope = ((double) (coordinates[i][1] - coordinates[i - 1][1])) / ((double) (coordinates[i][0] - coordinates[i - 1][0]));
+            if (curSlope != slope)
+                return false;
+        }
+
+        return true;
+    }
+
+}
