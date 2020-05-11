5fca9e7b8d3f2bf7d6cdcfb21558d97939836064
Peter Liang <peter_liang@outlook.com>
2020-05-11 18:31:07 +0800

    Optimized code.

--- java/src/com/peter/_2020/_30/days/trail/may/_11/Solution.java
+++ java/src/com/peter/_2020/_30/days/trail/may/_11/Solution.java
@@ -32,27 +32,21 @@ public class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         int oldColor = image[sr][sc];
         if (oldColor == newColor) return image;
-        
-        tryReplace(image, sr, sc, newColor, oldColor);
-
-        return image;
-    }
-
-    private void tryReplace(int[][] image, int sr, int sc, int newColor, int oldColor) {
-        if (image[sr][sc] != oldColor) return;
 
         image[sr][sc] = newColor;
-        if (sr > 0) {
-            tryReplace(image, sr - 1, sc, newColor, oldColor);
+        if (sr > 0 && image[sr - 1][sc] == oldColor) {
+            floodFill(image, sr - 1, sc, newColor);
         }
-        if (sr + 1 < image.length) {
-            tryReplace(image, sr + 1, sc, newColor, oldColor);
+        if (sr + 1 < image.length && image[sr + 1][sc] == oldColor) {
+            floodFill(image, sr + 1, sc, newColor);
         }
-        if (sc > 0) {
-            tryReplace(image, sr, sc - 1, newColor, oldColor);
+        if (sc > 0 && image[sr][sc - 1] == oldColor) {
+            floodFill(image, sr, sc - 1, newColor);
         }
-        if (sc + 1 < image[0].length) {
-            tryReplace(image, sr, sc + 1, newColor, oldColor);
+        if (sc + 1 < image[0].length && image[sr][sc + 1] == oldColor) {
+            floodFill(image, sr, sc + 1, newColor);
         }
+
+        return image;
     }
 }
