5fb32ba68dbf67a384bcda9f1ef8c1b47d653cbb
Peter Liang <peter_liang@outlook.com>
2020-05-20 13:00:07 +0800

    Solved problem may 2020 30 days trail problem 19.

--- java/src/com/peter/Application.java
+++ java/src/com/peter/Application.java
@@ -4,6 +4,10 @@ import com.peter._2020._30.days.trail.may._16_Odd_Even_Linked_List.Solution;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Application {
     public static void main(String[] args) {
@@ -15,8 +19,20 @@ public class Application {
     }
 
     private static void play() {
-        for (int i : "aA".chars().toArray()) {
-            System.out.println(i);
-        }
+        var list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(3);
+        list.add(3);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(1);
+        list.add(4);
+
+        Collections.sort(list);
+        System.out.println(Collections.binarySearch(list, 2));
+
+        list.forEach(System.out::println);
     }
 }
