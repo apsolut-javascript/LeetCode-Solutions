442e9e254243ae955e18fcbdf9386a8acf716f7d
Peter Liang <peter_liang@outlook.com>
2020-06-12 18:50:26 +0800

    Optimized problem for 2020 June 12 LeetCoding Challenge.

--- java/src/com/peter/_2020/_30/days/trail/june/_12_Insert_Delete_GetRandom/RandomizedSet.java
+++ java/src/com/peter/_2020/_30/days/trail/june/_12_Insert_Delete_GetRandom/RandomizedSet.java
@@ -1,38 +1,53 @@
 package com.peter._2020._30.days.trail.june._12_Insert_Delete_GetRandom;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomizedSet {
-    private final Set<Integer> set;
-
-    /**
-     * Initialize your data structure here.
-     */
-    public RandomizedSet() {
-        set = new HashSet<>();
+    public static void main(String[] args) {
+        var a = new RandomizedSet();
+        a.insert(1);
+        a.insert(2);
+        a.remove(1);
+        a.insert(3);
+        a.remove(2);
+        System.out.println("a.getRandom() = " + a.getRandom());
     }
+
+    private final Map<Integer, Integer> map = new HashMap<>();
+    private final List<Integer> list = new ArrayList<>();
 
     /**
      * Inserts a value to the set. Returns true if the set did not already contain the specified element.
      */
     public boolean insert(int val) {
-        return set.add(val);
+        if (map.containsKey(val)) return false;
+        map.put(val, list.size());
+        list.add(val);
+        return true;
     }
 
     /**
      * Removes a value from the set. Returns true if the set contained the specified element.
      */
     public boolean remove(int val) {
-        return set.remove(val);
+        if (!map.containsKey(val)) return false;
+        Integer index = map.get(val);
+        int tail = list.size() - 1;
+        if (index != tail) {
+            Integer element = list.get(tail);
+            Collections.swap(list, index, tail);
+            map.put(element, index);
+        }
+        list.remove(tail);
+        map.remove(val);
+        return true;
     }
 
     /**
      * Get a random element from the set.
      */
     public int getRandom() {
-        int i = ThreadLocalRandom.current().nextInt(set.size());
-        return set.stream().skip(i).findFirst().orElse(0);
+        return list.get(ThreadLocalRandom.current().nextInt(list.size()));
     }
 }
