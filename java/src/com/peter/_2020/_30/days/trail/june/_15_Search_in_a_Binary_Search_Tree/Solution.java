47b8820d887ccf5ec57913b343f41f622babae53
Peter Liang <peter_liang@outlook.com>
2020-06-15 18:09:46 +0800

    Optimized solution for 2020 June 15 LeetCoding Challenge.

--- java/src/com/peter/_2020/_30/days/trail/june/_15_Search_in_a_Binary_Search_Tree/Solution.java
+++ java/src/com/peter/_2020/_30/days/trail/june/_15_Search_in_a_Binary_Search_Tree/Solution.java
@@ -8,15 +8,11 @@ import java.util.Queue;
 public class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
         if (root == null) return null;
-        var queue = new ArrayDeque<TreeNode>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            var node = queue.removeLast();
-            if (node.val == val) return node;
-            if (node.left != null) queue.add(node.left);
-            if (node.right != null) queue.add(node.right);
+        if (root.val == val) return root;
+        var result = searchBST(root.left, val);
+        if (result == null) {
+            result = searchBST(root.right, val);
         }
-
-        return null;
+        return result;
     }
 }
