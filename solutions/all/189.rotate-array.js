1177cccef0a528bef3817ddab7772007de436961
Peter Liang <peter_liang@outlook.com>
2020-10-15 23:10:21 +0800

    Solved problem [189] Rotate Array

---
+++ solutions/all/189.rotate-array.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=189 lang=javascript
+ *
+ * [189] Rotate Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  k %= nums.length;
+  while (k-- > 0) nums.unshift(nums.pop());
+};
+// @lc code=end
